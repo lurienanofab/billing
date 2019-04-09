77b4eb93c32fd45c6654d54e4ae3e8eb1041caf8
jgett <jgett@umich.edu>
2019-04-09 15:08:51 -0400

    Adding existing project files.

---
+++ Billing/Controllers/RoomController.cs
@@ -0,0 +1,20 @@
+﻿using Billing.Models;
+using System.Web.Mvc;
+
+namespace Billing.Controllers
+{
+    public class RoomController : Controller
+    {
+        [Route("room/import-log")]
+        public ActionResult ImportLog()
+        {
+            return View();
+        }
+
+        [Route("room/import-log")]
+        public ActionResult ImportLog(ImportLogModel model)
+        {
+            return View(model);
+        }
+    }
+}
---
+++ Billing/Models/ImportLogModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Billing.Models
+{
+    public class ImportLogModel
+    {
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
